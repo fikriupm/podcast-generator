0da42f5096964a7eb1ead05e4ca6635de2e1880b
fikriupm <95086635+fikriupm@users.noreply.github.com>
2025-03-23 10:14:21 +0000

    update

--- feed.py
+++ feed.py
@@ -4,7 +4,7 @@ import xml.etree.ElementTree as xml_tree
 with open('feed.yaml', 'r') as file:
   yaml_data = yaml.safe_load(file)
 
-  rss_element = xml_tree.Element('rss', {'version':'2.0', 
+  rss_element = xml_tree.Element('rss', {'version':'2.0',
     'xmlns:itunes':'http://www.itunes.com/dtds/podcast-1.0.dtd',
     'xmlns:content':'http://purl.org/rss/1.0/modules/content/'})
 
@@ -24,20 +24,19 @@ xml_tree.SubElement(channel_element, 'link').text = link_prefix
 xml_tree.SubElement(channel_element, 'itunes:category', {'text': yaml_data['category']})
 
 for item in yaml_data['item']:
-  item_element = xml_tree.SubElement(channel_element, 'item')
-  xml_tree.SubElement(item_element, 'title').text = item['title']
-  xml_tree.SubElement(item_element, 'itunes:author').text = yaml_data['author']
-  xml_tree.SubElement(item_element, 'description').text = item['description']
-  xml_tree.SubElement(item_element, 'itunes:duration').text = item['duration']
-  xml_tree.SubElement(item_element, 'pubDate').text = item['published']
-  xml_tree.SubElement(item_element, 'title').text = item['title']
+    item_element = xml_tree.SubElement(channel_element, 'item')
+    xml_tree.SubElement(item_element, 'title').text = item['title']
+    xml_tree.SubElement(item_element, 'itunes:author').text = yaml_data['author']
+    xml_tree.SubElement(item_element, 'description').text = item['description']
+    xml_tree.SubElement(item_element, 'itunes:duration').text = item['duration']
+    xml_tree.SubElement(item_element, 'pubDate').text = item['published']
+    xml_tree.SubElement(item_element, 'title').text = item['title']
 
-  enclosure = xml_tree.SubElement(item_element, 'enclosure', {
-    'url': link_prefix + item['file'],
-    'type': 'audio/mpeg',
-    'length': item['length']
-  })
-
+    enclosure = xml_tree.SubElement(item_element, 'enclosure', {
+      'url': link_prefix + item['file'],
+      'type': 'audio/mpeg',
+      'length': item['length']
+    })
 
 output_tree = xml_tree.ElementTree(rss_element)
-output_tree.write('podcast.xml', encoding='UTF-8',xml_declaration=True)
+output_tree.write('podcast.xml', encoding='UTF-8', xml_declaration=True)
